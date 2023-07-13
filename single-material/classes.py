981b7738f569ce2e188e117a522d0ada58649331
Abolfazl <abolfazl.dmg@gmail.com>
2023-07-13 18:26:01 +0330

    Modify classes

--- single-material/classes.py
+++ single-material/classes.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 # Utility class for nodes
 class Node:
     def __init__(self, number, position, force, displacement):
@@ -8,12 +5,6 @@ class Node:
         self.position = position
         self.force = force
         self.displacement = displacement
-
-    def get_primary_variables(self, d, diff):
-        return np.array(d[diff[self.number]: diff[self.number] + len(self.displacement)])
-
-    def get_secondary_variables(self, f, diff):
-        return np.array(f[diff[self.number]: diff[self.number] + len(self.displacement)])
 
 
 class Element:
@@ -24,17 +15,3 @@ class Element:
         self.poisson = poisson
         self.thickness = thickness
         self.stress_mode = stress_mode
-
-    def get_xy(self, all_nodes):
-        X, Y = [], []
-        for p in self.nodes:
-            X.append(all_nodes[p].position[0])
-            Y.append(all_nodes[p].position[1])
-        return np.array(X).T, np.array(Y).T
-
-    def get_primary_variables(self, d, diff):
-        elem_d = []
-        for elem_node in self.nodes:
-            elem_d.append(d[diff[elem_node]])
-            elem_d.append(d[diff[elem_node] + 1])
-        return np.array(elem_d)

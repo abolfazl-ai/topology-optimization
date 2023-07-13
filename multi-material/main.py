36cdc071633203c90cfdcd5a027a605361dbcc9c
Abolfazl <abolfazl.dmg@gmail.com>
2023-07-13 20:06:05 +0330

    Added interpolation function

--- multi-material/main.py
+++ multi-material/main.py
@@ -4,7 +4,7 @@ from scipy.sparse import coo_matrix
 from utils import mesh, apply_bc, create_load_bc
 
 
-def main(length, width, n_elx, n_ely, vol_frac, penalty):
+def main(length, width, n_elx, n_ely, X0, r_min, vol_frac, cost_frac, penalty, D, E, P, MColor, MName, MinMove):
     dof = 2 * (n_elx + 1) * (n_ely + 1)
     e_dof = np.zeros((n_elx * n_ely, 8), dtype=int)
 
@@ -24,16 +24,20 @@ def main(length, width, n_elx, n_ely, vol_frac, penalty):
     jK = np.kron(e_dof, np.ones((1, 8))).flatten()
 
     KE = element_stiffness()
-    x = vol_frac * np.ones(elements.shape)
+    x = X0 * np.ones(elements.shape)
 
     plt.ion()
     loop, change = 0, 1
     while change > 0.001:
         loop += 1
         x_old = x.copy()
-        Un = fem(iK, jK, KE, U, F, x, penalty, dof)
+
+        E_, dE_ = ordered_simp_interpolation(n_elx, n_ely, x, penalty, D, E)
+        P_, dP_ = ordered_simp_interpolation(n_elx, n_ely, x, 1 / penalty, D, P)
+
+        Un = fem(iK, jK, KE, U, F, E_, penalty, dof)
         ce = (np.dot(Un[e_dof].reshape(n_elx * n_ely, 8), KE) * Un[e_dof].reshape(n_elx * n_ely, 8)).sum(1)
-        dc = ((-penalty * x.flatten() ** (penalty - 1)) * ce).reshape(x.shape)
+        dc = (-dE_.flatten() * ce).reshape(x.shape)
 
         x = oc(n_elx, n_ely, vol_frac, x, dc)
         change = np.max(abs(x - x_old))
@@ -46,11 +50,27 @@ def main(length, width, n_elx, n_ely, vol_frac, penalty):
     plt.pause(1E6)
 
 
-def fem(iK, jK, KE, U, F, x, penalty, dof):
-    elem_k = ((KE.flatten()[np.newaxis]).T * (x.flatten() ** penalty)).flatten(order='F')
+def fem(iK, jK, KE, U, F, E_int, penalty, dof):
+    elem_k = ((KE.flatten()[np.newaxis]).T * (E_int.flatten() ** penalty)).flatten(order='F')
     K = coo_matrix((elem_k, (iK, jK)), shape=(dof, dof)).tocsc()
     U = apply_bc(K, U, F)
     return U
+
+
+def ordered_simp_interpolation(n_elx, n_ely, x, penal, X, Y):
+    y = np.zeros((n_ely, n_elx))
+    dy = np.zeros((n_ely, n_elx))
+
+    for i in range(n_elx):
+        for j in range(n_ely):
+            for k in range(len(X) - 1):
+                if (X[k] < x[j, i]) and (X[k + 1] >= x[j, i]):
+                    A = (Y[k] - Y[k + 1]) / (X[k] ** (1 * penal) - X[k + 1] ** (1 * penal))
+                    B = Y[k] - A * (X[k] ** (1 * penal))
+                    y[j, i] = A * (x[j, i] ** (1 * penal)) + B
+                    dy[j, i] = A * penal * (x[j, i] ** ((1 * penal) - 1))
+                    break
+    return y, dy
 
 
 def oc(n_elx, n_ely, vol_frac, x, dc):
@@ -84,4 +104,10 @@ def element_stiffness(E=1, nu=0.3):
     return KE
 
 
-main(1, 1, 200, 200, 0.3, 3)
+D = [0, 0.4, 0.7, 1.0]
+E = [0, 0.2, 0.6, 1.0]
+P = [0, 0.5, 0.8, 1.0]
+MName = ['Void' 'A' 'B' 'C']
+MColor = ['w', 'b', 'r', 'k']
+main(1, 1, 200, 200, 0.5, 2.5, 0.3,
+     0.4, 3, D, E, P, MColor, MName, 0.001)

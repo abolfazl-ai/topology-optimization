b1bbde914b8b28a74006210d96173cba6898a006
Abolfazl <abolfazl.dmg@gmail.com>
2023-11-03 21:43:34 +0330

    Reformat

--- 3d-modified/test.py
+++
@@ -1,4 +0,0 @@
-teams_list = ["Man Utd", "Man City", "T Hotspur"]
-
-row_format = "{:>15}" * 4
-print(row_format.format("", *teams_list))
--- 3d/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from plot import Plot3D
-
-path = 'runs/45D1.73.npy'
-p = Plot3D(path, [0, 1], ['V', 'A'], ['w', 'b'])
-p.update(path, interactive=False)

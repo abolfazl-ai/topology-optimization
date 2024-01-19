5915c84c1df8b23f7cf4ed41a3ab5ee6b8fd636d
Abolfazl <abolfazl.dmg@gmail.com>
2024-01-19 11:07:22 +0330

    Starting data generation

--- machine_learning/generate_input.py
+++ machine_learning/generate_input.py
@@ -8,7 +8,7 @@ def generate_input(r, p, vol_fraction, boundary):
     materials = {'names': ['Void', 'Solid'], 'colors': ['w', 'k'], 'D': [1e-9, 1], 'E': [1e-9, 1]}
     mesh = mesh_model(400, 400, 0)
     filter_params = {'filter': 3, 'filter_bc': 'reflect', 'radius': r, 'eta': 0.2, 'beta': 2}
-    opt_params = {'penalty': p, 'penalty_increase': 0, 'volume_fraction': vol_fraction, 'move': 0.2,
+    opt_params = {'penalty': p, 'penalty_increase': 0.0, 'volume_fraction': vol_fraction, 'move': 0.2,
                   'max_it': 200, 'c_conv': 0.0001, 'x_conv': 0.001}
     pres, mask = np.zeros(mesh['shape']), np.ones(mesh['shape'], dtype=bool)
 
@@ -20,12 +20,14 @@ def gen_bc(mesh, bc):
     fixed, dof = [], mesh['dim'] * np.prod(np.array(mesh['shape']) + 1)
     force_vector = matrix(0.0, (dof, 1))
     for row in bc:
-        print(row)
         start, end, displacement, force = [row[key] for key in ('S', 'E', 'D', 'F')]
         nr = [(int(np.floor(s * n)), int(np.floor(e * n)) + 1) for n, s, e in list(zip(mesh['shape'], start, end))]
         nodes = (mesh['node_numbers'][nr[1][0]:nr[1][1], nr[0][0]:nr[0][1]]).flatten()
-        for i, (d, f) in enumerate(zip(displacement, force)):
-            force_vector[(mesh['dim'] * nodes + i).tolist(), 0] = matrix(0 if np.isnan(f) else f, (nodes.size, 1))
+        for i, d in enumerate(displacement):
+            if force != 0:
+                for node in nodes:
+                    y, x = np.argwhere(mesh['node_numbers'] == node)[0] / mesh['shape']
+                    force_vector[(mesh['dim'] * node + i).tolist(), 0] = force(x, y)[i]
             fixed.extend([] if np.isnan(d) else (mesh['dim'] * nodes + i).tolist())
     free = np.setdiff1d(np.arange(0, dof), fixed).tolist()
     return {'free_dofs': free, 'force_vector': force_vector}
---
+++ machine_learning/prepare_data.py
@@ -0,0 +1,78 @@
+from TopOpt import TopOpt
+from modes import *
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from machine_learning.generate_input import generate_input
+import os
+import csv
+
+
+def save_plot(ux, uy, u, c, save_path):
+    titles = ['Abs. Displacement X', 'Abs. Displacement Y', 'Absolute Displacement', 'Compliance']
+
+    fig, ax = plt.subplots(2, 2, figsize=(6, 6))
+    for i, (data, title) in enumerate(zip([np.abs(ux), np.abs(uy), u, c], titles)):
+        sns.heatmap(data, ax=ax[i // 2, i % 2], square=True, cmap='viridis', cbar=False,
+                    robust=True, xticklabels=False, yticklabels=False).invert_yaxis()
+        ax[i // 2, i % 2].set_title(title)
+
+    fig.savefig(save_path)
+
+
+def save_data(save_path, ux, uy, u, c):
+    np.save(arr=ux, file=save_path + '-X')
+    np.save(arr=uy, file=save_path + '-Y')
+    np.save(arr=u, file=save_path + '-U')
+    np.save(arr=c, file=save_path + '-C')
+    save_plot(ux, uy, np.sqrt(ux ** 2 + uy ** 2), c, save_path + '-inputs.png')
+    np.savetxt(save_path + '-inputs.csv', X=['InputsCode,Penalty,Radius,VolFrac'], fmt='%s', delimiter=',')
+
+
+def run(output_path='data'):
+    counter = 0
+    for bc_code, bc in bcs.items():
+        bc_path = output_path + '/' + bc_code
+        if not os.path.exists(bc_path): os.mkdir(bc_path)
+        for load_code, load in loads.items():
+            load_path = bc_path + '/' + load_code
+            if not os.path.exists(load_path): os.mkdir(load_path)
+            for penal in ps:
+                for r in rs:
+                    for vf in vfs:
+                        counter += 1
+                        code = f'{bc_code}-{load_code}-P{penal}-R{r:0.3f}-V{vf:0.3f}'
+                        print(f'Starting run {counter:06}: {code}')
+                        print(f'BC={bc_code}, Load={load_code}, P={penal}, R={r:0.3f}, VF={vf:0.3f}')
+
+                        optimizer = TopOpt(*generate_input(r, penal, vf, bc + load),
+                                           lambda loop, xx, x_ch, c_ch, cc, ww:
+                                           print(f'{loop:03}' + ' ' * 13 +
+                                                 f'{x_ch:<16.6f}{c_ch:<16.6f}{cc:<16.4E}{100 * ww:<0.2f}%'))
+
+                        if not os.path.exists(load_path + '/output'):
+                            initial_u = optimizer.fem.solve(np.ones(optimizer.mesh['shape']).flatten(order='F'))
+                            ux = initial_u[range(0, 2 * 401 * 401, 2)].reshape((401, 401), order='F')
+                            uy = initial_u[range(1, 2 * 401 * 401, 2)].reshape((401, 401), order='F')
+                            c = np.sum((initial_u[optimizer.mesh['c_mat']] @ optimizer.fem.k) *
+                                       initial_u[optimizer.mesh['c_mat']], 1).reshape((400, 400), order='F')
+
+                            save_data(load_path + '/' + bc_code + load_code, ux, uy, np.hypot(ux, uy), c)
+                            os.mkdir(load_path + '/output')
+
+                        optimizer.optimize()
+                        output = optimizer.x
+                        np.save(arr=output, file=load_path + '/output/' + code)
+
+                        with open(load_path + '/' + bc_code + load_code + '-inputs.csv', 'a', newline='') as file:
+                            writer = csv.writer(file, delimiter=',')
+                            writer.writerow([code, penal, r, vf])
+
+                        fig = plt.figure(figsize=(8, 8))
+                        sns.heatmap(1 - output, vmin=0, vmax=1, square=True, cmap='gray', cbar=False,
+                                    xticklabels=False, yticklabels=False).invert_yaxis()
+                        plt.title(f'BC={bc_code}, Load={load_code}, P={penal}, R={r:0.3f}, VF={vf:0.3f}')
+                        fig.savefig(load_path + '/output/' + code + '.png')
+
+
+run()
